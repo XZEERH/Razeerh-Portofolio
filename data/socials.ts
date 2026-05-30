c0c40cfeb083f30ca7298ef345167ca67221b978
XZEERH <razzyr408@gmail.com>
2026-05-30 14:45:31 +0700

    Create socials.ts

---
+++ data/socials.ts
@@ -0,0 +1,8 @@
+import { FaTiktok, FaInstagram, FaGithub, FaYoutube } from "react-icons/fa";
+
+export const socialsData = [
+  { name: "TikTok", icon: FaTiktok, link: "https://tiktok.com/@razeerh", color: "#ffffff" },
+  { name: "Instagram", icon: FaInstagram, link: "https://instagram.com/razeerh", color: "#E1306C" },
+  { name: "GitHub", icon: FaGithub, link: "https://github.com/razeerh", color: "#ffffff" },
+  { name: "YouTube", icon: FaYoutube, link: "https://youtube.com/@razeerh", color: "#FF0000" },
+];
