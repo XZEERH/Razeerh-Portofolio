0cbbc0b50ceb92018ec6a1cbf6a2a9a0da50e6dc
XZEERH <razzyr408@gmail.com>
2026-05-30 14:42:40 +0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: {
+          cyan: "#00f3ff",
+          blue: "#003cff",
+          purple: "#bc13fe",
+        },
+        dark: {
+          900: "#050505",
+          800: "#0a0a0a",
+          700: "#141414",
+        }
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
