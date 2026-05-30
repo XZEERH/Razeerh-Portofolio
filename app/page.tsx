0204ae25aa8ff0f4ecbee92debbbd4a301e74057
XZEERH <razzyr408@gmail.com>
2026-05-30 14:46:41 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "@studio-freight/react-lenis";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Stack from "@/components/Stack";
+import Projects from "@/components/Projects";
+import Socials from "@/components/Socials";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <main className="relative flex flex-col w-full overflow-hidden">
+      <Hero />
+      <About />
+      <Stack />
+      <Projects />
+      <Socials />
+      <Contact />
+    </main>
+  );
+}
