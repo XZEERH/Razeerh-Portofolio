92515f4053be967aa89b454420b1bc6d1c9609c1
XZEERH <razzyr408@gmail.com>
2026-06-01 18:11:26 +0700

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -1,26 +1,26 @@
 export const projectsData = [
   {
     id: 1,
-    title: "E-Commerce Futuristik",
-    description: "Platform toko online dengan desain modern, payment gateway, dan dashboard admin.",
-    link: "https://example.com",
-    github: "https://github.com",
-    tech: ["Next.js", "Tailwind", "Supabase"]
+    title: "Portal Astronomy",
+    description: "Platform portal online astronomi dengan desain modern, serta tempat pusat informasi alam semesta dan pendaftaran menjadi anggota EAC, dan dashboard admin.",
+    link: "https://portal-eac.vercel.app",
+    github: "https://github.com/XZEERH/EAC-PORTAL",
+    tech: ["HTML", "Tailwind", "Firebase"]
   },
   {
     id: 2,
-    title: "AI Chat Assistant",
-    description: "Aplikasi chat menggunakan berbagai API AI terintegrasi untuk membantu produktivitas.",
-    link: "https://example.com",
-    github: "https://github.com",
-    tech: ["React", "Node.js", "Google AI"]
+    title: "Platform musik streaming",
+    description: "web streaming musik menggunakan berbagai API streaming musik terintegrasi untuk membantu produktivitas.",
+    link: "https://azeerh-tify.vercel.app",
+    github: "https://github.com/XZEERH/AzeerhTify",
+    tech: ["TypeScript", "Next.js", "Google AI"]
   },
   {
     id: 3,
-    title: "Sistem Manajemen Inventori",
-    description: "Dashboard pengelolaan barang berbasis web dengan visualisasi data real-time.",
-    link: "https://example.com",
-    github: "https://github.com",
-    tech: ["Vue.js", "Firebase", "CSS3"]
+    title: "Detected Link Phising",
+    description: "Mendeteksi Link mencurigakan berbasis web dengan visualisasi data secara real-time.",
+    link: "https://azeerh-suspicious-detection.vercel.app",
+    github: "https://github.com/XZEERH/Azeerh-Detection-Link-Phising-And-Malware",
+    tech: ["Vue.js", "VirusTotal", "CSS3"]
   }
 ];
