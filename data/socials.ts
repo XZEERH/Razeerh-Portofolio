a2076d34a39834d143f0750b741c37d73d6a080c
XZEERH <razzyr408@gmail.com>
2026-06-01 15:08:24 +0700

    Update socials.ts

--- data/socials.ts
+++ data/socials.ts
@@ -1,8 +1,8 @@
 import { FaTiktok, FaInstagram, FaGithub, FaYoutube } from "react-icons/fa";
 
 export const socialsData = [
-  { name: "TikTok", icon: FaTiktok, link: "https://tiktok.com/@razeerh", color: "#ffffff" },
-  { name: "Instagram", icon: FaInstagram, link: "https://instagram.com/razeerh", color: "#E1306C" },
-  { name: "GitHub", icon: FaGithub, link: "https://github.com/razeerh", color: "#ffffff" },
-  { name: "YouTube", icon: FaYoutube, link: "https://youtube.com/@razeerh", color: "#FF0000" },
+  { name: "TikTok", icon: FaTiktok, link: "https://tiktok.com/@razeerh.edukasi", color: "#ffffff" },
+  { name: "Instagram", icon: FaInstagram, link: "https://instagram.com/razeerh.edukasi", color: "#E1306C" },
+  { name: "GitHub", icon: FaGithub, link: "https://github.com/XZEERH", color: "#ffffff" },
+  { name: "YouTube", icon: FaYoutube, link: "https://youtube.com/@RazeerhYA", color: "#FF0000" },
 ];
